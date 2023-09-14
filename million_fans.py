367aac615bb7cad4b50e84617527caa5ea85ca83
ColinHansen <136872394+ColinHansen@users.noreply.github.com>
2023-09-13 20:06:02 -0700

    Add files via upload

---
+++ million_fans.py
@@ -0,0 +1,7 @@
+sentence = "If X has a million fans, then I am one of them. If X has ten fans, then I am one of them. If X has only one fan, then that is me. If X has no fans, then that means I am no longer on earth. If the world is against X, then I am against the world."
+
+name = input("Enter a name: ")
+
+new_sentence = sentence.replace("X", name)
+
+print(new_sentence)
